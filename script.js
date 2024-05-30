b1029fc8e8126ef89c2430c462124bff11df02db
adrianna-yixin <esthergong499@gmail.com>
2024-05-30 15:08:22 +0100

    Add Quotes API

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
- 
+// Get Quotes From API
+// An asyncrounous function can run at any time independently and won't stop the browser from completely loading the page
+
+// Use "let" because we will change the value later on
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+    const apiUrl =
+      "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      newQuote();
+    } catch (error) {
+        console.error();
+    }
+}
+
+// On load
+getQuotes();
